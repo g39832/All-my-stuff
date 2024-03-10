2ae46b57480853dd13b88f04163837060ba1bff1
g39832 <142843776+g39832@users.noreply.github.com>
2024-03-10 17:09:28 -0500

    Create Calculator app.py

---
+++ Calculator app.py
@@ -0,0 +1,26 @@
+#This is just a basic calculater 
+import math
+
+# code that takes users inport
+
+number1 = int(input())
+sigh = input()
+number2 = int(input())
+
+if sigh == "+":
+    print (number1 + number2)
+
+if sigh == "-":
+    print (number1 - number2)
+    
+if sigh == "*":
+    print (number1 * number2)
+    
+if sigh == "/":
+    print (number1 / number2)
+    
+    
+    
+
+    
+    
